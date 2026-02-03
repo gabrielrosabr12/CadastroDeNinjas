d2915d07e8af78256990dcf790cb3f81eaf50a09
Gabriel Rosa <gabrielrosaobr@gmail.com>
2026-02-03 13:28:02 -0300

    Colocando construtores com e sem argumentos,getters e setters com Lombok, reduzindo o tamanho das classes com a Dependencia Lombok

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,28 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import jakarta.persistence.*;
+import java.util.List;
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name= "tb_cadastro_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+    // @OneToMany - Uma missão pode ter muitos ninjas
+    @OneToMany(mappedBy = "missoes") // Mapear a coluna da tabela ninja
+    private List<NinjaModel> ninjas;
+
+}
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,8 +1,11 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
 
-/// JPA significa Java Persistence API
+////JPA significa Java Persistence API
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 /// Anotar com @Entity para declarar que esta classe é uma entidade do banco de dados
 /// <br>Para isso iremos baixar uma dependencia nova SpringJPA
@@ -15,45 +18,24 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_cadastro_de_ninjas")
+@Data // Lombok cria todos os getters e os setters
+@NoArgsConstructor //Lombok cria um construtor sem argumentos invisivel
+@AllArgsConstructor // Lombok cria um construtor com todos argumentos
 public class NinjaModel {
 
     @Id //será o id primary key do db, é da biblioteca jakarta.persiste
-    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoincrement a partir do numero 1, passamos a estratégia
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoincrement a partir do número 1, passamos a estratégia
     private Long id;
+
     private String nome;
+
     private int idade;
+
     private String email;
 
-
-    public NinjaModel(){}
-
-    public NinjaModel(String nome, int idade, String email){
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    // @ManyToOne - Um ninja tem uma unica missão
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreign Key chave estrangeira
+    private MissoesModel missoes;
 }
+
