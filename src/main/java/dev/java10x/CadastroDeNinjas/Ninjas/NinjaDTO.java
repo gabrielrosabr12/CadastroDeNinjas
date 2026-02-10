f00c9a915f9bc3113d79a9b269d03e4643c3a626
Gabriel <gabrielrosaobr@gmail.com>
2026-02-10 13:38:05 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    /// Somente uma classe qualquer que terá os mesmos atributos da entidade , DTO vai conversar com Service
+    private Long id;
+    private String nome;
+    private int idade;
+    private String email;
+    private String imgUrl;
+    private String rank;
+    private MissoesModel missoes;
+
 }
