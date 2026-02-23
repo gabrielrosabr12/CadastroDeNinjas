dca3d6dfef38c76775f6c0c0efefccb4c576dfc8
Gabriel Rosa <gabrielrosaobr@gmail.com>
2026-02-23 08:02:14 -0300

    feat: Implementa UI para mostrar detalhes do ninja, Adiciona no NinjaControllerUI a conexão com esse html, ajusta o btn-detalhes do  listarninjas.html

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUI.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaControllerUI.java
@@ -1,5 +1,7 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,6 +30,17 @@ public class NinjaControllerUI {
         return "listarninjas";
     }
 
+    @GetMapping("/listar/{id}")
+    public String listarPorId(@PathVariable Long id, Model model) {
+        NinjaDTO ninjaDTO = ninjaService.listarNinjasPorId(id);
+
+        if (ninjaDTO != null) {
+            model.addAttribute("ninja",ninjaDTO);
+
+            return "listarninjaid";
+        }
+        return "redirect:/ninjas/ui/listar";
+    }
 
 
     @GetMapping("/delete/{id}")
