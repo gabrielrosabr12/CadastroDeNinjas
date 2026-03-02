38a547cd4e13e8bbd7d6b424ce0680f9b7f70bc1
Gabriel Rosa <gabrielrosaobr@gmail.com>
2026-03-02 07:55:50 -0300

    feat: Implementa a dependencia SpringSecurity, cria uma configuração basica em SecurityConfig

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package dev.java10x.CadastroDeNinjas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    /* UserDetailsService é o servico principal que recupera informações de seguranca sobre os usuários.
+     * Como Definimos nosso bean personalizado, não vemos mais no log a senha padrão fornecida pelo
+     * Spring, pois agora estamos fornecendo a nossa */
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        /* UserDetails é a interface que o spring usa para processar informações de segurança do usuário
+        * como nome,senha  (consulte a classe para obter mais informações) estamos usando a classe
+        * User padrão como implementação*/
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}senha") //{noop} evita uso de codificador
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
