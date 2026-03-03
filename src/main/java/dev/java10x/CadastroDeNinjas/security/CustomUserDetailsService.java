22809688b8a4ccfaaf5bb0a7c42d05468d844fa4
Gabriel <gabrielrosaobr@gmail.com>
2026-03-03 17:12:37 -0300

    fix: Adiciona o usuario na tabela no banco de dados, cria a classe de Usuario, a interface UserRepository para a Service do usuario poder consumir

--- src/main/java/dev/java10x/CadastroDeNinjas/security/CustomUserDetailsService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/security/CustomUserDetailsService.java
@@ -1,5 +1,7 @@
 package dev.java10x.CadastroDeNinjas.security;
 
+import dev.java10x.CadastroDeNinjas.CustomUser.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,13 +9,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
-        return User.builder()
-                .username("user")
-                .password("$2a$12$f1zJ0wN13vgH0xwXEbdS7.p6tyDGr1GCXLcboBKwJ0hHFJ178Dv/q")
-                .build();
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Ninja não encontrado!"));
     }
 }
---
+++ src/main/resources/db/migrations/V3__Add_user_default_to_Custom_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO CUSTOM_USER (username,password)
+VALUES ('user','$2a$12$sz0MbESXpSoND7d1S/8ob.VlZIgxe.aS2vjuautZof1SONSqBiUmW')
